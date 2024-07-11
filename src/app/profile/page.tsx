4f79d7b1b4243c4a31c44a68d66412d352f21d9c
Hye-Kyung9 <rngprud123@naver.com>
2024-07-11 13:43:27 +0900

    fixed untracked files

--- src/app/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-/** @jsxImportSource @emotion/react */
-'use client';
-import { css } from '@emotion/react';
-
-export default function Profile() {
-  return <div css={css({ backgroundColor: 'yellow' })}>노오랑</div>;
-}
