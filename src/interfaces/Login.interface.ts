895d0fb8f3c785e265b732487048598af8bcee1a
Hye-Kyung9 <rngprud123@naver.com>
2024-07-11 13:39:18 +0900

    feat:next-auth설치 / 인증로직 구현 / env파일 수정

---
+++ src/interfaces/Login.interface.ts
@@ -0,0 +1,12 @@
+export interface LoginReq {
+  loginId: string;
+  loginPassword: string;
+}
+
+// export interface LoginRes extends CommonResponse {
+//   userInfo: {
+//     loginId: string;
+//     name: string;
+//     permission: string;
+//   };
+// }
