d0e5ded7392cd90b6d8663d984272760a5aea69f
Hye-Kyung9 <rngprud123@naver.com>
2024-07-13 23:27:31 +0900

    feat: tailwind설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,23 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#60A5FA',
+        secondary: '#9ca3af',
+        third: '#0064ff',
+        normal: '#111',
+        success: '#111',
+        failure: '#ef4444',
+      },
+      animation: {
+        actSlideRight: 'slidRight 0.5s forwards',
+      },
+      screens: {
+        md: '1000px',
+      },
+      width: {
+        500: '500px',
+      },
     },
   },
 };
